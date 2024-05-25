2cbe826cacd9c0d95143df166879478b6aab17d5
Aliev Madjit <madzitaliev51@gmail.com>
2024-05-25 19:21:45 +0600

    Создал функцию клика, сброса и вывед все в App

--- homeWork-54/src/App.tsx
+++ homeWork-54/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Item } from './types'
+import Board from './components/Cell/Board/Board';
 
 function App() {
   const createCells =  () => {
@@ -17,12 +18,37 @@ function App() {
   }
 
   const [cells, setCells] = useState<Item[]>(createCells());
+  const [attempts, setAttempts] = useState<number>(0);
+
+  const handleCellClick = (index: number) => {
+  if (!cells[index].clicked) {
+    const newCells = [...cells];
+    const newCell = { ...newCells[index] };
+    newCell.clicked = true;
+    newCells[index] = newCell;
+    setAttempts(attempts + 1);
+    setCells(newCells);
+    if (newCell.hasItem) {
+      alert("Игра окончена");
+    }
+  }
+};
+
+const handleReset = (): void => {
+    setCells(createCells());
+    setAttempts(0);
+  };
 
 
   return (
     <>
-    <div>
-      
+    <div className='game'>
+      <h1>Игра</h1>
+      <Board items={cells} onCellClick={handleCellClick} />
+      <p>Попыток: {attempts}</p>
+      <button className="reset-button" onClick={handleReset}>
+        Сбросить игру
+      </button>
     </div>
     </>
   );
