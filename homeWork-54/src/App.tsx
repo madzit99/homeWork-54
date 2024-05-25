616ff2e2074f3b7168b2ded0d30b7eebde5c678f
Aliev Madjit <madzitaliev51@gmail.com>
2024-05-25 18:05:10 +0600

    Создал функцию, которая создает масив из 36 обьектов

--- homeWork-54/src/App.tsx
+++ homeWork-54/src/App.tsx
@@ -1,8 +1,31 @@
-import './App.css'
+import { useState } from 'react';
+import { Item } from './types'
 
 function App() {
+  const createCells =  () => {
+    const items: Item[] = [];
+    for (let i = 0; i < 36; i++) {
+      items.push({
+        hasItem: false,
+        clicked:false,
+      });
+    }
 
-  return ()
+    const indexRandom = Math.floor(Math.random() * items.length);
+    items[indexRandom].hasItem = true;
+    return items;
+  }
+
+  const [cells, setCells] = useState<Item[]>(createCells());
+
+
+  return (
+    <>
+    <div>
+      
+    </div>
+    </>
+  );
 }
 
 export default App
